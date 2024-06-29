c7c6a0846d9d642cdae2322cedc79afc75cad767
dgl <2245731645@qq.com>
2024-06-29 21:32:06 +0800

    两个封号

---
+++ springboot/src/main/java/com/example/springboot/entity/Student.java
@@ -0,0 +1,15 @@
+package com.example.springboot.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 22457
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+}
