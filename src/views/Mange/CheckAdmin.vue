63c0bf8b21b448540cb1292a33782549ee02fe5a
dgl <2245731645@qq.com>
2024-06-30 14:23:19 +0800

    页面优化

--- src/views/Mange/CheckAdmin.vue
+++ src/views/Mange/CheckAdmin.vue
@@ -48,37 +48,34 @@
 
 
 <!--    添加成员弹窗-->
-      <el-dialog title="添加成员" :visible.sync="addAdminLog" width="50%" center>
-        <h2 style="text-align: center">新增成员</h2>
-        <div class="login-box">
-          <el-form  label-position="labelPosition" label-width="80px"  model="form">
-            <el-form-item label="姓名" style="width: 300px;">
-              <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
-            </el-form-item>
-            <el-form-item  label="年龄" style="width: 300px;">
-              <el-input v-model="form.age" placeholder="请输入年龄"></el-input>
-            </el-form-item>
-            <br>
-            <el-form-item  label="性别">
-              <!--       <el-input v-model="form.sex" placeholder="请输入性别"></el-input>-->
-              <el-radio v-model="form.sex" label="男">男</el-radio>
-              <el-radio v-model="form.sex" label="女">女</el-radio>
-            </el-form-item>
-            <br>
-            <el-form-item label="地址" style="width: 500px;">
-              <el-input v-model="form.address" placeholder="请输入地址"></el-input>
-            </el-form-item>
-            <br>
-            <el-form-item label="电话" style="width: 500px;">
-              <el-input v-model="form.phone" placeholder="请输入电话"></el-input>
-            </el-form-item>
-          </el-form>
-        </div>
-        <div style="text-align: center">
-          <el-button type="primary"  @click="addAdmin">提交</el-button>
-          <!--      <el-button type="danger">取消</el-button>-->
-        </div>
-      </el-dialog>
+    <el-dialog title="添加成员" :visible.sync="addAdminLog" width="50%" center>
+      <div style="padding: 20px ;margin: 20px;width: 400px" class="form-area">
+        <el-form  label-position="" label-width="80px" :model="form" inline="true" style="width: 600px;margin-left:auto;">
+          <el-form-item label="姓名">
+            <el-input v-model="form.name" placeholder="请输入姓名"></el-input>
+          </el-form-item>
+          <el-form-item  label="年龄">
+            <el-input v-model="form.age" placeholder="请输入年龄"></el-input>
+          </el-form-item>
+          <el-form-item  label="性别">
+            <el-radio v-model="form.sex" label="男">男</el-radio>
+            <el-radio v-model="form.sex" label="女">女</el-radio>
+          </el-form-item>
+        </el-form>
+        <el-form  label-position="left" label-width="80px" :model="form" style="width: 510px;margin-left:40px;">
+          <el-form-item label="地址">
+            <el-input v-model="form.address" placeholder="请输入地址"></el-input>
+          </el-form-item>
+          <el-form-item label="电话">
+            <el-input v-model="form.phone" placeholder="请输入电话"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div style="text-align: center">
+        <el-button type="primary" @click="addAdmin" >提交</el-button>
+        <!--      <el-button type="danger">取消</el-button>-->
+      </div>
+    </el-dialog>
 
 <!--    修改信息-->
     <div>
@@ -96,7 +93,7 @@
             <el-radio v-model="form.sex" label="女">女</el-radio>
           </el-form-item>
         </el-form>
-        <el-form  label-position="top" label-width="80px" :model="form" style="width: 600px;margin-left:40px;">
+        <el-form  label-position="left" label-width="80px" :model="form" style="width: 510px;margin-left:40px;">
           <el-form-item label="地址">
             <el-input v-model="form.address" placeholder="请输入地址"></el-input>
           </el-form-item>
@@ -194,7 +191,7 @@ export default {
       request.post("/user/addAdmin",this.form).then(res => {
         if(res.code === "success"){
           this.$notify.success('新增成功')
-          this.form={}
+          this.refresh()
         }
         else{
           this.$notify.error(res.msg);
@@ -206,6 +203,7 @@ export default {
         if(res.code === "success"){
           this.$notify.success('更新成功')
           this.editAdminLog=false
+          this.refresh()
         }
         else{
           this.$notify.error(res.msg);
@@ -217,6 +215,9 @@ export default {
       request.get("/user/" +low.id ).then(res => {
         this.form = res.data;
       })
+    },
+    refresh() {
+      location.reload();
     }
   }
 }
